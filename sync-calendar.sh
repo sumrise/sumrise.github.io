ce28c991e51e1c4bd297c0369cb3c0abea41b365
sumrise <303371827@qq.com>
2018-01-26 15:13:45 +0800

    x

--- sync-calendar.sh
+++ sync-calendar.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 echo 'start'
-rm -rf basic.ics
+
 wget https://calendar.google.com/calendar/ical/7je3t02hd7r2oei4c0osmgs8fo%40group.calendar.google.com/private-ee632e40105552ff877304e2af938a72/basic.ics
 
-git add -A
-git commit -m 'x'
-git push origin master
+DIFF="$(diff basic.ics basic.ics.1 |grep 'END')"
+if [ "$DIFF" != "" ]; then
+    echo "DIFFERENT"
+    echo "$DIFF"
+    rm -rf basic.ics
+    mv basic.ics.1 basic.ics
+
+    git add -A
+    git commit -m 'x'
+    git push origin master
+
+fi
+
+echo "end"
